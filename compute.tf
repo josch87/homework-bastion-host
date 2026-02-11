87e3d60a43e3ea593fa966c11c4957a8439efd8d
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-11 18:15:36 +0100

    Add AWS EC2 bastion host resource to Terraform configuration

---
+++ compute.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "bastion_host" {
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type = "t3.small"
+  key_name      = "vockey"
+  subnet_id = aws_subnet.homework_public_subnet.id
+
+  tags = {
+    Name = "Bastion Host"
+  }
+}
