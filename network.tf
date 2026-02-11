1d26f8d27917a0a9ae3f1099d35aaddc4f13d713
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-11 17:56:27 +0100

    Add public route table and association to Terraform configuration

--- network.tf
+++ network.tf
@@ -31,3 +31,21 @@ resource "aws_internet_gateway" "homework_ig" {
     Name = "Homework IG"
   }
 }
+
+resource "aws_route_table" "homework_public_rtb" {
+  vpc_id = aws_vpc.homework_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.homework_ig.id
+  }
+
+  tags = {
+    Name = "Homework Public RTB"
+  }
+}
+
+resource "aws_route_table_association" "public_rtb_association" {
+  subnet_id      = aws_subnet.homework_public_subnet.id
+  route_table_id = aws_route_table.homework_public_rtb.id
+}
