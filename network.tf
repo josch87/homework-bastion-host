abdb51de9a05fabe87b96122daf6bd3ea9fef7f8
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-11 20:39:52 +0100

    Add private route table and association to Terraform configuration

--- network.tf
+++ network.tf
@@ -71,3 +71,21 @@ resource "aws_nat_gateway" "homework_nat_gateway" {
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.homework_ig]
 }
+
+resource "aws_route_table" "homework_private_rtb" {
+  vpc_id = aws_vpc.homework_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.homework_nat_gateway.id
+  }
+
+  tags = {
+    Name = "Homework Private RTB"
+  }
+}
+
+resource "aws_route_table_association" "private_rtb_association" {
+  subnet_id      = aws_subnet.homework_private_subnet.id
+  route_table_id = aws_route_table.homework_private_rtb.id
+}
